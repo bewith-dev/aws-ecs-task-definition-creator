25f27399744ef221f6ba91c7983f3231477f3f65
Eyal Levin <eyal.levin@bewith.io>
2022-02-06 19:37:07 +0200

    init project

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-aws-ecs-task-definition-creator
+# Aws Ecs Task Definition Creator
+
+****
+Full input list [action](./action.yml)
+
+## Task Definition Not Implemented support
+
+***
+
+- [ ] volumes
+- [ ] placementConstraints
+- [ ] containerDefinitions
+- [ ] Support multiple containers
+
+## TODO
+
+***
+
+- [ ] Add tests
+- [ ] Split to container class and task definition class
+- [ ] Automate build with GitHub actions
+
+## Status
+
+***
+
+The project is under active development and may have breaking changes till version 1.0.0.
+
+we are trying our best not to break things.
+
+
+### Inspiration
+From  [aws-actions/amazon-ecs-render-task-definition](https://github.com/aws-actions/amazon-ecs-render-task-definition)
+
---
+++ src/index.js
@@ -0,0 +1,225 @@
+const core = require('@actions/core');
+const tmp = require('tmp');
+const fs = require('fs');
+
+export class TaskDefinition {
+    _container = {
+        name: "",
+        image: "",
+        memoryReservation: 0,
+        cpu: 0,
+        environment: [],
+        environmentFiles: [],
+        portMappings: [],
+        logConfiguration: {
+            logDriver: "",
+            options: {}
+        }
+    };
+
+    _family = "";
+    _taskRoleArn = "";
+    _executionRoleArn = "";
+
+    constructor() {
+        this.containerName = core.getInput('container-name', {required: true});
+        this.containerImage = core.getInput('image', {required: true});
+
+        /** Environment Variables */
+        this.containerEnvironmentVariables = core.getInput('environment-variables', {required: false});
+        this.containerEnvironmentVariablesFiles = core.getInput('environment-variables-files', {required: false});
+
+        this.family = core.getInput('family', {required: false});
+        this.taskRoleArn = core.getInput('task-role-arn', {required: false});
+        this.executionRoleArn = core.getInput('execution-role-arn', {required: false});
+
+        this.containerMemoryReservation = core.getInput('memory-reservation', {required: false});
+        this.containerCpu = core.getInput('cpu', {required: false});
+
+        /** Port Mapping */
+        this.containerPortMappingTcp = core.getInput('port-mapping-tcp', {required: false});
+        this.containerPortMappingUdp = core.getInput('port-mapping-udp', {required: false});
+
+        /** Logs */
+        this.containerLogDriver = core.getInput('log-driver', {required: false});
+        this.containerLogDriverOptions = core.getInput('log-driver-options', {required: false});
+    }
+
+    build() {
+        const updatedTaskDefFile = tmp.fileSync({
+            tmpdir: process.env.RUNNER_TEMP,
+            prefix: 'task-definition-',
+            postfix: '.json',
+            keep: true,
+            discardDescriptor: true
+        });
+
+        const taskDef = {
+            containerDefinitions: [
+                this.container
+            ],
+            family: this.family,
+            taskRoleArn: this.taskRoleArn,
+            executionRoleArn: this.executionRoleArn,
+            volumes: [],
+            placementConstraints: [],
+        };
+
+        const newTaskDefContents = JSON.stringify(taskDef, null, 2);
+        fs.writeFileSync(updatedTaskDefFile.name, newTaskDefContents);
+        core.setOutput('task-definition', updatedTaskDefFile.name);
+    }
+
+    get container() {
+        return this._container;
+    }
+
+
+    get taskRoleArn() {
+        return this._taskRoleArn;
+    }
+
+    get executionRoleArn() {
+        return this._executionRoleArn;
+    }
+
+    set taskRoleArn(value) {
+        this._taskRoleArn = value;
+    }
+
+    set executionRoleArn(value) {
+        this._executionRoleArn = value;
+    }
+
+    get family() {
+        return this._family;
+    }
+
+    set family(value) {
+        this._family = value;
+    }
+
+    set containerCpu(name) {
+        this._container.cpu = name;
+    }
+
+    set containerMemoryReservation(value) {
+        this._container.memoryReservation = value;
+    }
+
+    set containerName(name) {
+        this._container.name = name;
+    }
+
+    set containerImage(image) {
+        this._container.image = image;
+    }
+
+    set containerLogDriver(driver) {
+        this._container.logConfiguration.logDriver = driver;
+    }
+
+    set containerEnvironmentVariablesFiles(files) {
+        files.split('\n').forEach((line) => {
+            const trimmedLine = line.trim();
+            if (trimmedLine.length === 0) {
+                return;
+            }
+
+            const variable = {
+                type: 's3',
+                value: trimmedLine,
+            };
+
+            const variableDef = this._container.environmentFiles.find((e) => {
+                return e.value === variable.value;
+            });
+            if (variableDef) {
+                variableDef.value = variable.value;
+            } else {
+                this._container.environmentFiles.push(variable);
+            }
+        });
+    }
+
+    set containerEnvironmentVariables(variables) {
+        variables.split('\n').forEach((line) => {
+            const trimmedLine = line.trim();
+            if (trimmedLine.length === 0) {
+                return;
+            }
+
+            const separatorIdx = trimmedLine.indexOf("=");
+            if (separatorIdx === -1) {
+                throw new Error(`Cannot parse the environment variable '${trimmedLine}'. Environment variable pairs must be of the form NAME=value.`);
+            }
+
+            const variable = {
+                name: trimmedLine.substring(0, separatorIdx),
+                value: trimmedLine.substring(separatorIdx + 1),
+            };
+
+            const variableDef = this._container.environment.find((e) => {
+                return e.name === variable.name
+            });
+
+            if (variableDef) {
+                variableDef.value = variable.value;
+            } else {
+                this._container.environment.push(variable);
+            }
+        });
+    }
+
+    set containerLogDriverOptions(options) {
+        let optionsParsed = {};
+        options.split('\n').forEach((option) => {
+            const logDriverOption = option.trim();
+            if (logDriverOption.length === 0) {
+                return;
+            }
+            const logDriverOptionSplit = logDriverOption.split("=");
+            optionsParsed[logDriverOptionSplit[0]] = logDriverOptionSplit[1];
+        });
+
+        this._container.logConfiguration.options = optionsParsed;
+    }
+
+    set containerPortMappingTcp(portMappingTcp) {
+        portMappingTcp.split('\n').forEach((ports) => {
+            const portsTrimmed = ports.trim();
+            if (portsTrimmed.length === 0) {
+                return;
+            }
+            const portsSplit = portsTrimmed.split("=");
+            this._container.portMappings.push({
+                containerPort: parseInt(portsSplit[0]),
+                hostPort: parseInt(portsSplit[1]),
+                protocol: "tcp"
+            });
+        });
+    }
+
+    set containerPortMappingUdp(portMappingUdp) {
+        portMappingUdp.split('\n').forEach((ports) => {
+            const portsTrimmed = ports.trim();
+            if (portsTrimmed.length === 0) {
+                return;
+            }
+            const portsSplit = portsTrimmed.split("=");
+            this._container.portMappings.push({
+                containerPort: parseInt(portsSplit[0]),
+                hostPort: parseInt(portsSplit[1]),
+                protocol: "udp"
+            });
+        });
+    }
+}
+
+
+try {
+    const taskDefinition = new TaskDefinition();
+    taskDefinition.build();
+} catch (error) {
+    core.setFailed(error.message);
+}
